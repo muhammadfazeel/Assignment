6314eb642b2b60e10137786c00e94e8c30afd069
KaShMiRi <p.pakistan47@gmail.com>
2018-07-13 22:07:29 +0500

    Complete

--- Assignment.js
+++ Assignment.js
@@ -5,5 +5,45 @@ function AddRecord(Id,Name,Address,Email,Class)
     var Add =[Id,Name,Address,Email,Class];
     Main.push(Add);
 }
-AddRecord("1","Fazeel","AJ&K","p.pakistan47@gmail.com","BS(SE)");
-console.log(Main);
+//To Remove Record...
+function RemoveRecord(Id)
+{
+Main.map(function(Value)
+{
+if(Value[0]===Id)
+{
+    delete Main[0];
+}
+});
+}
+//To Update Record...
+function UpdateRecord(Id)
+{
+    Main.map(function(Value)
+    {
+        if(Value[0]===Id)
+        {
+            Value[1]="Jamal Khan";
+            console.log("Updated Record = : "+Value);
+        }
+
+    });
+}
+//Display Record...
+function DisplayREcord(Id)
+{
+    Main.map(function(Value)
+    {
+        if(Value[0]===Id)
+        {
+            console.log("Display Record of Specific Id : "+Value);
+        }
+    });
+}
+
+AddRecord(1,"Fazeel","AJ&K","p.pakistan47@gmail.com","BS(SE)");
+AddRecord(2,"Jamal","Abbotabad","jimikhan@gmail.com","BS(SE)");
+AddRecord(3,"Zohaib","RawalPinid","Zohaib@gmail.com","BS(SE)");
+RemoveRecord(1);
+UpdateRecord(2);
+DisplayREcord(3);
