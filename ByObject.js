5b4d935bff4ea1ef9915e4bbcc954a5ee5b848b8
KaShMiRi <p.pakistan47@gmail.com>
2018-07-14 18:23:58 +0500

    Delete Array

--- ByObject.js
+++ ByObject.js
@@ -1,4 +1,5 @@
 var Main=[];
+//To Add Record...
 function AddRecord(Id,Name,Address,Email,Class)
 {
     Main.push(
@@ -12,6 +13,15 @@ function AddRecord(Id,Name,Address,Email,Class)
     
 );
 }
+//To Delete Record..
+function DeleteRecord(Id)
+{
+  var key=Id;
+  delete Main[key];
+}
 
-AddRecord(1,"Fazeel","AJ&K","p.pakistan47@gmail.com","BS(SE)");
+AddRecord(0,"Fazeel","AJ&K","p.pakistan47@gmail.com","BS(SE)");
+AddRecord(1,"Uxama","Mirpur AJ&K","uxama@gmail.com","BS(CV)");
+AddRecord(2,"Zohaib","Rawalpindi","Zohaib@gmail.com","BS(SE)");
+DeleteRecord(0);
 console.log(Main);
