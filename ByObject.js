97bd232ba88fb531749b54ab1c8c37aaa7b29acb
KaShMiRi <p.pakistan47@gmail.com>
2018-07-14 18:40:59 +0500

    Complete

--- ByObject.js
+++ ByObject.js
@@ -19,9 +19,31 @@ function DeleteRecord(Id)
   var key=Id;
   delete Main[key];
 }
+//To Update Record...
+function UpdateRecord(Id)
+{
+Main.map(function(Value)
+{
+Value[1]="Adeel";
+Value[2]="PodriChok";
+Value[3]="Adeel@gmail.com";
+});
+}
+function DisplayREcord()
+{
+    Main.map(function(Value)
+    {
+        console.log(Value);
+    });
+}
+
+
+
 
 AddRecord(0,"Fazeel","AJ&K","p.pakistan47@gmail.com","BS(SE)");
 AddRecord(1,"Uxama","Mirpur AJ&K","uxama@gmail.com","BS(CV)");
 AddRecord(2,"Zohaib","Rawalpindi","Zohaib@gmail.com","BS(SE)");
 DeleteRecord(0);
+UpdateRecord(1);
+DisplayREcord();
 console.log(Main);
